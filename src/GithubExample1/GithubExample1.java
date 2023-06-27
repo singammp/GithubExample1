860844a0bdb3f056251cea01aef4f8f8d0678363
singammp <narasingam.m96@gmail.com>
2023-06-27 12:20:29 +0530

    first commit

---
+++ src/GithubExample1/GithubExample1.java
@@ -0,0 +1,15 @@
+package GithubExample1;
+
+public class GithubExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GithubExample1 git = new GithubExample1();
+		int result = git.addTwoNumbers(1, 3);
+		System.out.println("result >> " + result);
+	}
+
+	public int addTwoNumbers(int a, int b) {
+		return (a + b);
+	}
+}
